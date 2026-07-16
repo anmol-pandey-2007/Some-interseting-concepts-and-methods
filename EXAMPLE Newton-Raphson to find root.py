f3f8cbf4fd1761cdbeb51a8cd8852b369490f0f9
anmol-pandey-2007 <anmol.pandey2025@lpu.in>
2026-07-17 00:22:00 +0530

    Update EXAMPLE Newton-Raphson to find root.py

--- EXAMPLE Newton-Raphson to find root.py
+++ EXAMPLE Newton-Raphson to find root.py
@@ -1,6 +1,6 @@
 #EXAMPLE: Newton-Raphson to find roots
 e = 0.01
-k = 2265
+k = 2265 # reference number 
 guess = k/2.0
 num_guesses = 0
 
@@ -9,3 +9,6 @@ while abs(guess*guess - k) >= e:
 	guess = guess - (((guess**2) - k)/(2*guess))
 print(f'num_guesses = {str(num_guesses)}')
 print(f'Square root of {str(k)} is about {str(guess)}')
+#formula used: x(n)=x(n-1)-f{x(n)-1}/f'{x(n)-1} 
+#x(n) is the next guess
+#here the equation is f(x)=x^2-k
